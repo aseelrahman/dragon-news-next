718777ab18c8bb9aaf485d4f95a4f89ce79a783b
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-07-20 22:52:29 -0400

    display news card and handle loading

--- src/app/(main)/category/[id]/page.jsx
+++ src/app/(main)/category/[id]/page.jsx
@@ -1,15 +1,13 @@
 import LeftSidebar from "@/components/homepage/news/LeftSidebar";
+import NewsCard from "@/components/homepage/news/NewsCard";
 import RightSidebar from "@/components/homepage/news/RightSidebar";
 import { getCategories, getNewsByCategoryId } from "@/lib/data";
 import React from "react";
-
-
 
 const NewsCategoryPage = async ({ params }) => {
   const { id } = await params;
 
   const categories = await getCategories();
-
   const news = await getNewsByCategoryId(id);
 
   return (
@@ -17,16 +15,18 @@ const NewsCategoryPage = async ({ params }) => {
       <div className="col-span-3">
         <LeftSidebar categories={categories} activeId={id} />
       </div>
-      <div className="font-bold col-span-6">
+      <div className="col-span-6">
         <h2 className="font-bold text-lg">News by category</h2>
         <div className="space-y-4 mt-6">
-          {news.length > 0? news.map((n) => {
-            return (
-              <div key={n._id} className="p-6 rounded-md border">
-                {n.title}
-              </div>
-            );
-          }) : <h2 className="font-bold text-4xl text-center my-7">No news found!</h2>}
+          {news.length > 0 ? (
+            news.map((n) => {
+              return <NewsCard key={n._id} news={n} />;
+            })
+          ) : (
+            <h2 className="font-bold text-4xl text-center my-7">
+              No news found!
+            </h2>
+          )}
         </div>
       </div>
       <div className="col-span-3">
